19faa33cd8cdd95ad067060e1bc9a1f759268c69
cha <zktmzkxh1234@gmail.com>
2025-04-11 15:10:50 +0900

    kyoungjun commit before relation hotfix

--- src/main/java/com/unique/dto/RoomDTO.java
+++ src/main/java/com/unique/dto/RoomDTO.java
@@ -1,30 +1,21 @@
 package com.unique.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.*;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RoomDTO {
-    private Long roomSeq; //방 시퀀스
-    private String roomName; //방 이름
-    private Long userSeq; //방 생성 유저명
-    private Character viewYn; //방 공개여부
-    private String roomPw; //방 비밀번호
-    private Integer limitTime; //제한시간
-    private Integer limitCnt; //인원제한
-    private Character activeYn; //활성여부
-    private String roomStatus; //방상태_진행대기완료
-    private String shutdownYn; //강제종료 여부
-    private Long examSeq; // 시험지 시퀀스
-    private Long subjectSeq; // 과목코드
-    private String subjectName; // 과목명
+    private Long roomSeq;
+    private String title;
+
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date regdate;
 
-
+    //시험 방관리
+    private List<ExamDTO> examList;
 }
