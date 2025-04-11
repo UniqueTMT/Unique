700974ad02ce16cac3554ca5e37c92c316096dc7
cha <zktmzkxh1234@gmail.com>
2025-04-11 11:15:07 +0900

    kyoungjun commit before merge pull relation hotfix

--- src/main/java/com/unique/dto/RoomDTO.java
+++ src/main/java/com/unique/dto/RoomDTO.java
@@ -1,6 +1,7 @@
 package com.unique.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.*;
 import java.util.Date;
@@ -12,17 +13,16 @@ public class RoomDTO {
     private Long roomSeq; //방 시퀀스
     private String roomName; //방 이름
     private Long userSeq; //방 생성 유저명
-    private String viewYn; //방 공개여부
+    private Character viewYn; //방 공개여부
     private String roomPw; //방 비밀번호
-    private Date limitTime; //제한시간
-    private Long limitCnt; //인원제한
-
-
-//            ACTIVE_YN
-//    REGDATE
-//            ROOM_STATUS
-//    SHUTDOWN_YN
-
+    private Integer limitTime; //제한시간
+    private Integer limitCnt; //인원제한
+    private Character activeYn; //활성여부
+    private String roomStatus; //방상태_진행대기완료
+    private String shutdownYn; //강제종료 여부
+    private Long examSeq; // 시험지 시퀀스
+    private Long subjectSeq; // 과목코드
+    private String subjectName; // 과목명
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date regdate;
 
