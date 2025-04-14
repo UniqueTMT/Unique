a961e682479dd14e60f9b5e84638057227c72b47
이제무 <jemu0120@gmail.com>
2025-04-14 10:53:20 +0900

    [feat] #40 화면 흐름 초기 설정.

---
+++ src/main/java/com/unique/dto/member/FindIdRequestDTO.java
@@ -0,0 +1,8 @@
+package com.unique.dto.member;
+
+import lombok.Data;
+@Data
+public class FindIdRequestDTO {
+    private String name;
+    private String email;
+}
---
+++ src/main/java/com/unique/dto/member/FindPwRequestDTO.java
@@ -0,0 +1,8 @@
+package com.unique.dto.member;
+
+import lombok.Data;
+@Data
+public class FindPwRequestDTO {
+    private String userId;
+    private String email;
+}
